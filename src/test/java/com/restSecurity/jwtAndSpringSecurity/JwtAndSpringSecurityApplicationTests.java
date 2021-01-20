4605c4c86d77c625dc16393ee69312d884bc39c0
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2021-01-20 00:44:48 -0300

    Project created

---
+++ src/test/java/com/restSecurity/jwtAndSpringSecurity/JwtAndSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restSecurity.jwtAndSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAndSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
