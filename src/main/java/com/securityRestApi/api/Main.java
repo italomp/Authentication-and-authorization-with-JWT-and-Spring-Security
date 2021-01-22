c1b2025aa4aecb166f3754deb6099fdb432bce7f
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2021-01-21 23:56:56 -0300

    maven project ok

--- src/main/java/com/securityRestApi/api/Main.java
+++ src/main/java/com/securityRestApi/api/Main.java
@@ -1,13 +1,22 @@
 package com.securityRestApi.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
